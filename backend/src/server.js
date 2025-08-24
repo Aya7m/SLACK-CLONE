d9e6dd11ed3aef326a41cfc679bd8e9d4038a806
Aya7m <yyoyo6987@gmail.com>
2025-08-24 23:57:14 +0300

    db connected and env

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,10 @@
 import express from 'express'
-import {ENV} from './config/env.js'
+import { ENV } from './config/env.js'
+import { connectDB } from './config/conectionDB.js'
 const app = express()
 
 
 app.get('/', (req, res) => res.send('Hello World!'))
-app.listen(ENV.PORT, () => console.log(`Example app listening on port ${ENV.PORT}!`))
+app.listen(ENV.PORT, () => console.log(`Example app listening on port ${ENV.PORT}!`),
+    connectDB()
+)
