c522d66a7e4609fd70102c4e34faaa44b93e9982
Aya7m <yyoyo6987@gmail.com>
2025-08-24 22:49:47 +0300

    install

---
+++ backend/src/server.js
@@ -0,0 +1,7 @@
+import express from 'express'
+import {ENV} from './config/env.js'
+const app = express()
+
+
+app.get('/', (req, res) => res.send('Hello World!'))
+app.listen(ENV.PORT, () => console.log(`Example app listening on port ${ENV.PORT}!`))
