a15fd087b32de44e50960dc49202ebd98ef0be70
Aya7m <yyoyo6987@gmail.com>
2025-08-26 10:55:36 +0300

    auth setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,23 +3,25 @@ import React from 'react'
 import { Navigate, Route, Routes } from 'react-router-dom'
 import HomePage from './pages/HomePage'
 import AuthPage from './pages/AuthPage'
+import * as Sentry from "@sentry/react";
 
+const SentryRoutes = Sentry.withSentryReactRouterV7Routing(Routes);
 const App = () => {
   return (
     <>
-    <SignedIn>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/auth" element={<Navigate to={"/"} replace />} />
-       </Routes>
-     </SignedIn>
+      <SignedIn>
+        <SentryRoutes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/auth" element={<Navigate to={"/"} replace />} />
+        </SentryRoutes>
+      </SignedIn>
 
-     <SignedOut>
-    <Routes>
-        <Route path="/auth" element={<AuthPage />} />
-        <Route path="*" element={<Navigate to={"/auth"} replace />} />
-        </Routes>
-    </SignedOut>       
+      <SignedOut>
+        <SentryRoutes>
+          <Route path="/auth" element={<AuthPage />} />
+          <Route path="*" element={<Navigate to={"/auth"} replace />} />
+        </SentryRoutes>
+      </SignedOut>
     </>
   )
 }
